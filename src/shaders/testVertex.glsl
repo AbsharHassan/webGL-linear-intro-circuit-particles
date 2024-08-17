57d49b26e05a5e2afae8a40b0405b1489e5bb743
AbsharHassan <abshar.hassan7@gmail.com>
2024-08-17 06:52:07 +0500

    working (apparently) complete transition to using shaderMaterial

--- src/shaders/testVertex.glsl
+++ src/shaders/testVertex.glsl
@@ -1,18 +1,24 @@
 precision highp float;
 
 attribute vec3 pos;
+// attribute mat4 instanceMatrix;
 
 varying vec2 vUv;
 
 void main() {
     vUv = uv;
 
-    vec3 item_position = (modelMatrix * vec4(pos + position, 1.0)).xyz;
-
-    vec4 view_pos = viewMatrix * vec4(item_position, 1.0);
-
-    // view_pos.xyz += position * 1.0;
-
-    gl_Position = projectionMatrix * view_pos;
-
+    vec4 worldPosition = instanceMatrix * vec4(position, 1.0);
+    gl_Position = projectionMatrix * modelViewMatrix * worldPosition;
 }
+
+// void main() {
+//     vUv = uv;
+
+//     vec3 item_position = (modelMatrix * vec4(pos + position, 1.0)).xyz;
+
+//     vec4 view_pos = viewMatrix * vec4(item_position, 1.0);
+
+//     gl_Position = projectionMatrix * view_pos;
+
+// }
