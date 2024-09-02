254ec43f66294953b5979ad5ba0086f29caf9992
AbsharHassan <abshar.hassan7@gmail.com>
2024-09-02 07:38:12 +0500

    working single noise value vertices export with overlapping vertices merged using the merge by distance node. However, the file size is larger because the numbers have a higher accuracy, need to deal w this issue

--- src/Components/CircuitParticlesInstancedMeshes.jsx
+++ src/Components/CircuitParticlesInstancedMeshes.jsx
@@ -13,11 +13,14 @@ import {
 import gsap from 'gsap'
 
 import { circuitVertices } from '../circuitVertices'
-import { optimizedVerticesV1 as optimizedVertices } from '../optimizedCircuitVerticesV1'
+// import { experiment } from '../experiment'
+import { optimizedVerticesV1 } from '../optimizedCircuitVerticesV1'
+import { optimizedVerticesV2 } from '../optimizedCircuitVerticesV2'
 import { degToRad } from 'three/src/math/MathUtils.js'
 
 import iMeshCircuitLinesFragShader from '../shaders/iMeshCircuitLineShaders/iMeshCircuitLinesFragment.glsl'
 import iMeshCircuitLinesVertShader from '../shaders/iMeshCircuitLineShaders/iMeshCircuitLinesVertex.glsl'
+import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
 
 const heightSegments = 64
 const widthSegments = 64
@@ -37,6 +40,15 @@ const lineMaterialOld = new THREE.LineBasicMaterial({
   color: 0xff0000,
 })
 
+const meshLineMat = new MeshLineMaterial({
+  lineWidth: 0.0125,
+  transparent: true,
+  depthTest: false,
+  depthWrite: false,
+  opacity: 0.4,
+  // fragmentShader: circuitLinesFragment,
+})
+
 const CircuitParticlesInstancedMeshes = () => {
   const { scene, viewport, gl } = useThree()
 
@@ -52,10 +64,34 @@ const CircuitParticlesInstancedMeshes = () => {
   useEffect(() => {
     const startTime = performance.now() // Start timing
 
+    // console.log(experiment)
+
+    // setTimeout(() => {
+    //   console.log('start')
+    //   iterateNoiseLines()
+    // }, 5000)
+
+    let lineMeshs = []
+
     let oldLines = []
 
-    optimizedVertices.forEach((pathArray) => {
-      oldLines.push(addLines(pathArray, lineMaterialOld))
+    // optimizedVertices.forEach((pathArray) => {
+    //   oldLines.push(addLines(pathArray, lineMaterialOld))
+    // })
+
+    optimizedVerticesV2.map((pathArray, index) => {
+      // if (index !== 0) {
+      //   return
+      // }
+
+      const meshLineGeo = new MeshLineGeometry()
+      // const linePoints = vertices.map(({ x, y }) => [x, y])
+      meshLineGeo.setPoints(pathArray)
+      const meshLineMesh = new THREE.Mesh(meshLineGeo, meshLineMat)
+
+      scene.add(meshLineMesh)
+
+      lineMeshs.push(meshLineMesh)
     })
 
     const endTime = performance.now() // End timing
@@ -67,8 +103,42 @@ const CircuitParticlesInstancedMeshes = () => {
           scene.remove(line)
         })
       }
+      if (lineMeshs.length) {
+        lineMeshs.forEach((line) => {
+          scene.remove(line)
+        })
+      }
     }
   }, [])
+
+  // const iterateNoiseLines = () => {
+  //   const progress = {
+  //     i: 1,
+  //   }
+
+  //   let lines = []
+
+  //   gsap.to(progress, {
+  //     i: 400,
+  //     duration: 20,
+  //     ease: 'linear',
+  //     onUpdate: () => {
+  //       if (lines.length) {
+  //         lines.forEach((line) => {
+  //           scene.remove(line)
+  //         })
+  //       }
+
+  //       let index = Math.floor(progress.i)
+  //       console.log(index)
+  //       let vertices = experiment[index - 1]
+
+  //       vertices.forEach((pathArray) => {
+  //         lines.push(addLines(pathArray, lineMaterialOld))
+  //       })
+  //     },
+  //   })
+  // }
 
   const addLines = (vertices, lineMaterial) => {
     let points = []
